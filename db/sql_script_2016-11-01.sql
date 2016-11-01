80c3c03c6e83704d954f04b115d524a799deb40a
yudetao <yudetao@hsbank360.com>
2016-11-01 12:27:26 +0800

    init

---
+++ db/sql_script_2016-11-01.sql
@@ -0,0 +1,51 @@
+drop table if exists user;
+create table if not exists user(
+	id bigint(20) not null auto_increment comment '编号',
+	username varchar(50) not null comment '用户名',
+	password varchar(100) not null comment '密码',
+	name varchar(50) default null comment '姓名',
+	sale varchar(20) default null comment '性别',
+	date_of_birth date default null comment '出生日期',
+	mobile varchar(20) default null comment '手机号',
+	enail varchar(100) default null comment '邮箱',
+	nickname varchar(50) default null comment '昵称',
+	level int(11) default null comment '等级',
+	experience int(11) default null comment '经验值',
+	topic_number int(11) default null comment '发帖数',
+	fans_number int(11) default null comment '粉丝数量',
+	concern_number int(11) default null comment '关注数量',
+	label_ids varchar(255) default null comment '标签',
+	primary key (id)
+);
+alter table user comment '用户';
+
+drop table if exists topic;
+create table if not exists topic(
+	id bigint(20) not null auto_increment comment '编号',
+	user_id bigint(20) not null comment '发帖人',
+	name varchar(255) not null comment '名称',
+	content text not null comment '内容',
+	description varchar(255) default null comment '描述',
+	category_id bigint(20) default null comment '类别',
+	label_ids varchar(255) default null comment '标签',
+	status varchar(50) default null comment '状态',
+	create_datetime datetime not null comment '发帖时间',
+	last_reply_datetime datetime default null comment '最后回复时间',
+	view_times int(11) default null comment '浏览量',
+	zan_times int(11) default null comment '点赞数',
+	primary key (id)
+);
+alter table topic comment '帖子';
+
+drop table if exists reply;
+create table if not exists reply(
+	id bigint(20) not null auto_increment comment '编号',
+	topic_id bigint(20) not null comment '帖子编号',
+	reply_id bigint(20) default null comment '回复编号',
+	content text not null comment '内容',
+	create_datetime datetime not null comment '回复时间',
+	agree_times int(11) default null comment '赞成数量',
+	disagree_times int(11) default null comment '不赞成数量',
+	primary key (id)
+);
+alter table reply comment '回复';
